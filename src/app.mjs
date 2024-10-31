d034064c49e3adc8176ab4589f9a8c5093783adf
mkphung29 <mkp6075@nyu.edu>
2024-10-31 15:17:26 -0400

    Set up app skeleton

--- src/app.mjs
+++ src/app.mjs
@@ -1,9 +1,16 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Middleware
+app.use(express.json());
 
 app.listen(process.env.PORT || 3000);
