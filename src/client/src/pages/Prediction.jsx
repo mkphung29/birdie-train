858993a0cb3abe7db60bdd28dcb45e416ad9d4c6
mkphung29 <109439396+mkphung29@users.noreply.github.com>
2025-04-12 23:16:48 -0400

    Remove trailing slash in  Prediction.jsx

--- src/client/src/pages/Prediction.jsx
+++ src/client/src/pages/Prediction.jsx
@@ -18,7 +18,7 @@ export default function Prediction() {
 
         try {
             //const response = await fetch('http://localhost:8080/api/predictions', {
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/api/predictions`, {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}api/predictions`, {
                 method: 'POST',
                 headers: { 
                     'Content-Type': 'application/json',
